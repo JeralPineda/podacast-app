5460a00ac7ce2ba24d42573fa5dc83477e31ba35
Jeral Pineda <jeral.cris@gmail.com>
2026-04-16 22:01:00 -0600

    fix: authentication using Clerk

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,39 @@
 import { Stack } from "expo-router";
 
+import { ClerkProvider, useAuth } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
 function RootStack() {
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" />
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Protected guard={isSignedIn}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" />
+      </Stack.Protected>
     </Stack>
   );
 }
 
 export default function RootLayout() {
-  return <RootStack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <RootStack />
+    </ClerkProvider>
+  );
 }
