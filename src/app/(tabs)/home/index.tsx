a363db801f62caa93f37b94e50c9756f74cc3903
Jeral Pineda <jeral.cris@gmail.com>
2026-04-18 16:13:55 -0600

    fix: podcast index setup and first call

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { fetchTrending } from "@/services/podcast-index";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
+  const onPress = async () => {
+    const data = await fetchTrending();
+    // console.log("🚀 index.tsx -> #7 ~ data:", data);
+    console.log("🚀 index.tsx -> #7 ~ data:", JSON.stringify(data, null, 2));
+  };
+
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <Button title="Fetch trending" onPress={onPress} />
     </View>
   );
 }
---
+++ src/services/podcast-index.ts
@@ -0,0 +1,54 @@
+import { Feed } from "@/types";
+import * as Crypto from "expo-crypto";
+
+// Había un problema con la api secret al parecer cortaba la variable por culpa del signo $, eso me devolvia un 401 en la api.
+// Como se resolvió
+// 1. se ejecuto la siguiente linea en la terminal: echo -n 'Valor de la variable' | base64
+// 2. se copia el resultado y se pone en la variable de entorno de expo: EXPO_PUBLIC_PODCAST_INDEX_API_SECRET_B64=<resultado_del_comando>
+
+const apiKey = process.env.EXPO_PUBLIC_PODCAST_INDEX_API_KEY;
+// const apiSecret = process.env.EXPO_PUBLIC_PODCAST_INDEX_API_SECRET;
+const apiSecret = process.env.EXPO_PUBLIC_PODCAST_INDEX_API_SECRET_B64;
+
+if (!apiKey || !apiSecret) {
+  throw new Error(
+    "EXPO_PUBLIC_PODCAST_INDEX_API_KEY or EXPO_PUBLIC_PODCAST_INDEX_API_SECRET is not defined",
+  );
+}
+
+const fetchIndex = async (path: string, options: RequestInit = {}) => {
+  // Read more about Podcast Index Authorization: https://podcastindex-org.github.io/docs-api/#auth
+
+  // ======== Hash them to get the Authorization token ========
+  const time = Math.floor(Date.now() / 1000);
+  const dataToHash = apiKey + "BJ$b2xpJbHkWyGxG^D6C9^jakNrs2ubyWW#86Fev" + time;
+
+  const authHash = await Crypto.digestStringAsync(Crypto.CryptoDigestAlgorithm.SHA1, dataToHash);
+
+  const optionsWithAuth = {
+    ...options,
+    method: options.method || "get",
+    headers: {
+      "User-Agent": "notJustPodcast/1.0",
+      "X-Auth-Date": "" + time,
+      "X-Auth-Key": apiKey,
+      Authorization: authHash,
+      ...options.headers,
+    },
+  };
+
+  const url = `https://api.podcastindex.org/api/1.0${path}`;
+
+  return fetch(url, optionsWithAuth);
+};
+
+export async function fetchTrending(): Promise<{ feeds: Feed[] } | undefined> {
+  try {
+    const res = await fetchIndex(`/podcasts/trending`);
+
+    return res.json();
+  } catch (error) {
+    console.log("🚀 podcast-index.ts -> #47 -> error ~", error);
+    // return { feeds: [] };
+  }
+}
